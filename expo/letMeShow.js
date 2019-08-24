3422b0d27021d983f8ad668fec17c72897ffc4b3
Alisson Santos <45664013+alissin4444@users.noreply.github.com>
2019-08-24 19:55:43 -0300

    Update letMeShow.js

--- expo/letMeShow.js
+++ expo/letMeShow.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Image } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState('Let me show what are you missing');
+  const [state, setState] = useState({
+    text: "Let me show what are you missing",
+    value: false,
+  });
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="deixar" onPress={ () => setText("Paradise") }/>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>{state.text}</Text>
+        <View style={{marginTop: 20}}>
+          {state.value &&
+            <Image 
+              style={{width: 200, height: 200}}
+              source={{uri: "https://data.whicdn.com/images/332471200/large.jpg"}}
+            />
+          }
+        </View>
+        <View style={{marginTop: 20}}>
+          {!state.value && 
+            <Button
+              title="Yes"
+              color="#8c14fc"
+              onPress={() => setState({text: "Paradise", value: true})}
+            />
+          }
+        </View>
+      </View>
+    </>
   );
 }
 
@@ -17,5 +38,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
